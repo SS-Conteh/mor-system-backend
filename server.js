c0159bf05b9b998be439648687bc04d3ae4fcca7
SS-Conteh <contehsantoss1@gmail.com>
2026-04-15 20:03:45 +0000

    Fix duplicate cloudinary declaration

--- server.js
+++ server.js
@@ -265,19 +265,6 @@ const roleMiddleware = (...roles) => {
   };
 };
 
-// ========== MULTER + CLOUDINARY CONFIG ==========
-
-// Cloudinary Setup
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-
-// Configure Cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 // Profile Photo Upload (Images)
 const profileStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
